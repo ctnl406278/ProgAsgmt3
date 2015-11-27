5431eb153a3058af3e730fb86e65be23e033a015
ctnl406278 <ctnl406278@yahoo.com>
2015-11-27 10:40:34 +0900

    Started 1st task to find "the best hospital in the state". Created "best.R" and added function description.

---
+++ best.R
@@ -0,0 +1,8 @@
+#The "best.R" function determines the hospital that has the best (lowest) 30-day
+#mortality for a specified outcome in that state. The current outcomes being
+#assessed are (1) "heart attack", (2) "heart failure" and (3) "pneumonia".
+
+best <- function(state, outcome){
+    
+    
+}
